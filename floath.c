ef76b5543f5ba43d298c4a5926da3109ac1053dd
Newton Galiza <newtonjgaliza@gmail.com>
2019-02-20 15:05:40 -0300

    Create floath.c

---
+++ floath.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <float.h>
+
+ int main()
+ 
+ {
+ 
+     ptintf("Storage size for float: %d \n", sizeof(float));
+     printf("Maximum float value: %E \n", FLT_MAX);
+     printf("Minimum float value: %E \n", FLT_MIN);
+     printf("Precision value: %d \n", FLT_DIG);
+     
+     return 0; 
+ 
+ }
