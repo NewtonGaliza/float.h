3e41e9c240e7babd058f7073857595ec4f3efd54
Newton Galiza <newtonjgaliza@gmail.com>
2019-02-20 15:10:37 -0300

    Update floath.c

--- floath.c
+++ floath.c
@@ -5,7 +5,7 @@
  
  {
  
-     ptintf("Storage size for float: %d \n", sizeof(float));
+     printf("Storage size for float: %d \n", sizeof(float));
      printf("Maximum float value: %E \n", FLT_MAX);
      printf("Minimum float value: %E \n", FLT_MIN);
      printf("Precision value: %d \n", FLT_DIG);
